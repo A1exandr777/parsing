b867cea06105ce5258734d81ddf8768241a2ce33
A1exandr777 <levenskih007@mail.ru>
2023-03-07 23:31:03 +0500

    дз

--- ДЗ/3 модуль/Углубленный анализ HTML кода, Веб-краулеры.py
+++ ДЗ/3 модуль/Углубленный анализ HTML кода, Веб-краулеры.py
@@ -1,7 +1,18 @@
+import re
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 html = urlopen("https://docs.python.org/3/library/index.html")
-bs = BeautifulSoup(html,"html.parser")
+bs = BeautifulSoup(html, "html.parser")
+links = bs.find_all("a")
+for i in links:
+    a = i.get("href")
+    match = re.search(r"https.*", a)
+    list = [match[0] if match else None]
+    for b in range(len(list)):
+        if list[b] == None:
+            continue
+        else:
+            print(list[b])
 
-links = bs.find_all("div",{"class":"toctree-wrapper compound"})
+
